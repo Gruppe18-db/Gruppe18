79a09cb5b7e1a5b5d035649098fdd896a7d72eaf
Sasha2610 <ishmatova.oleks-24@stud.dhbw-ravensburg.de>
2026-04-27 14:58:14 +0200

    Add registration button to login form

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <h1>Startseite</h1>
 
 <h2>Login Rennveranstalter</h2>
-<form method="post" action="login.php">
+<form method="post" action="login_renveranstalter.php">
     <label>Name:</label><br>
     <input type="text" name="name" required><br><br>
     
@@ -22,5 +22,9 @@
     <input type="password" name="password" required><br><br>
     
     <button type="submit">Anmelden</button>
+</form>
+
+<form method="get" action="register_rennveranstalter.php">
+    <button type="submit">Registrieren</button>
 </form>
 
