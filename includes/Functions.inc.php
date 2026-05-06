ba0e5daa82e9ecd144058b767ff2f05ed6895350
D-Oeztuerk <dilara-oeztuerk@outlook.com>
2026-05-06 12:40:08 +0200

    Add files via upload
    neue functions datei

---
+++ includes/Functions.inc.php
@@ -0,0 +1,69 @@
+<?php
+// Autor: Dilara Öztürk
+
+function createTeamchef($pdo, $LoginnameTC, $Kennwort, $VornameTC, $NachnameTC) {
+    $hash = password_hash($Kennwort, PASSWORD_DEFAULT);
+    $stmt = $pdo->prepare("
+    INSERT INTO Teamchef (Loginname, VornameTC, NachnameTC, Kennwort)
+    VALUES (:Loginname, :VornameTC, :NachnameTC, :Kennwort)"
+    );
+    $stmt->execute([
+        'Loginname' => $LoginnameTC,
+        'VornameTC' => $VornameTC,
+        'NachnameTC' => $NachnameTC,
+        'Kennwort' => $hash
+    ]);
+}
+
+function createTeam($pdo, $TeamName, $LoginnameTC) {
+    $stmt = $pdo->prepare("INSERT INTO Team (TeamName, LoginnameTC) VALUES (:TeamName, :LoginnameTC)"
+    );
+    $stmt->execute(['TeamName' => $TeamName, 'LoginnameTC' => $LoginnameTC]);
+}
+
+function teamExists($pdo, $TeamName) {
+    $stmt = $pdo->prepare("
+    SELECT COUNT(*) 
+    FROM Team 
+    WHERE TeamName = ?"
+    );
+    $stmt->execute([$TeamName]);
+    return $stmt->fetchColumn() > 0;
+}
+
+function saveCyclist($pdo, $TeamName, $MitarbeiterID, $VornameF, $NachnameF, $Strasse, $Hausnummer, $PLZ, $Ort, $Telefonnummer) {
+    if ($MitarbeiterID !== null) {
+        $stmt = $pdo->prepare("
+            UPDATE Fahrer
+            SET VornameF = ?, NachnameF = ?, Strasse = ?, Hausnummer = ?, PLZ = ?, Ort = ?, Telefonnummer = ?
+            WHERE MitarbeiterID = ? AND TeamName = ?
+        ");
+        $stmt->execute([$VornameF, $NachnameF, $Strasse, $Hausnummer, $PLZ, $Ort, $Telefonnummer, $MitarbeiterID, $TeamName]);
+    } else {
+        $stmt = $pdo->prepare("
+            INSERT INTO Fahrer (TeamName, VornameF, NachnameF, Strasse, Hausnummer, PLZ, Ort, Telefonnummer)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ");
+        $stmt->execute([$TeamName, $VornameF, $NachnameF, $Strasse, $Hausnummer, $PLZ, $Ort, $Telefonnummer]);
+    }
+}
+
+function getCyclist($pdo, $TeamName) {
+    $stmt = $pdo->prepare("
+    SELECT *
+    FROM Fahrer
+    WHERE TeamName = ?"
+    );
+    $stmt->execute([$TeamName]);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function deleteCyclist($pdo, $MitarbeiterID, $TeamName) {
+    $stmt = $pdo->prepare("
+    DELETE FROM Fahrer
+    WHERE MitarbeiterID = ? AND TeamName = ?"
+    );
+    $stmt->execute([$MitarbeiterID, $TeamName]);
+}
+
+?>
