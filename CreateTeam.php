be6a94e680690485467c17cba3c4794f842b201b
D-Oeztuerk <dilara-oeztuerk@outlook.com>
2026-05-19 11:31:58 +0200

    Fix include paths in CreateTeam.php
    / gelöscht

--- CreateTeam.php
+++ CreateTeam.php
@@ -1,8 +1,8 @@
 <?php
 // Autor: Dilara Öztürk
 
-include '/includes/db.inc.php';
-include '/includes/Functions.inc.php';
+include 'includes/db.inc.php';
+include 'includes/Functions.inc.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
