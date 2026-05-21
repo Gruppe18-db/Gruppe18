2137767e77cc29128d114ec269a51b695344e5dc
D-Oeztuerk <dilara-oeztuerk@outlook.com>
2026-05-21 07:53:59 +0200

    Kommentare eingefügt

--- LoginTeamchef.php
+++ LoginTeamchef.php
@@ -4,22 +4,22 @@ ini_set('display_errors', 1); error_reporting(E_ALL);
 session_start();
 include 'includes/db.inc.php';
 
-if (!empty($_POST['LoginnameTC']) && !empty($_POST['Kennwort'])) {
-
+if (!empty($_POST['LoginnameTC']) && !empty($_POST['Kennwort'])) { // Kontrolle ob ie Felder ausgefüllt sind
+    // Die eingeegebenen Daten werden gespeichert
     $LoginnameTC = $_POST['LoginnameTC'];
     $Kennwort = $_POST['Kennwort'];
-
+    // Datenbankabfrage zur Kontrolle der Logindaten
     $stmt = $pdo->prepare("SELECT tc.Loginname, 
     tc.Kennwort,
     t.TeamName
     FROM Teamchef tc, Team t 
-    WHERE tc.Loginname = t.LoginnameTC AND tc.Loginname = :Loginname"
+    WHERE tc.Loginname = t.LoginnameTC AND tc.Loginname = :Loginname" // :Loginname als Platzhalter zum Schutz vor SQL Injection
     );
-
-    $stmt->execute(['Loginname' => $LoginnameTC]);
+    
+    $stmt->execute(['Loginname' => $LoginnameTC]); // Der Platzhalter :Loginname wird mit dem eingegebenen Loginnamen ersetzt
     $teamchef = $stmt->fetch();
 
-    if($teamchef && password_verify($Kennwort, $teamchef['Kennwort'])) {
+    if($teamchef && password_verify($Kennwort, $teamchef['Kennwort'])) { // Das Passwort wird geprüft
         
         $_SESSION['teamchef_logged_in'] = true;
         $_SESSION['LoginnameTC'] = $teamchef['Loginname'];
